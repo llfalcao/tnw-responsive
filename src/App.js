b4e3ba6463aff4a21a5aa331ab5905d019782dac
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-11-10 20:34:16 -0300

    Add videos and partners section

--- src/App.js
+++ src/App.js
@@ -245,6 +245,114 @@ const App = () => {
             </li>
           </ul>
         </section>
+
+        <section className="videos">
+          <div className="videos__main"></div>
+          <div className="videos__list">
+            {/* TODO: fetch latest videos from the TNW youtube channel */}
+          </div>
+        </section>
+
+        <section className="partners">
+          <div className="top">
+            <header className="partners__header">
+              <h6 className="partners__subHeading">REACH YOUR GOALS</h6>
+              <h2 className="partners_heading">Work with us</h2>
+              <p className="partners__intro">
+                TNW takes center stage in the tech industry, offering creative
+                media campaigns, sizzling tech events, bespoke innovation
+                programs, and prime office locations in Amsterdam. Want to
+                engage your audience more effectively, innovate your business,
+                and position your brand in front of a digital-savvy audience?
+              </p>
+              <button className="partners_cta" type="button">
+                GET IN TOUCH
+              </button>
+            </header>
+
+            <div className="partners__grid">
+              <div className="partners__gridItem">
+                <img
+                  src="https://s3.amazonaws.com/events.tnw/hardfork-2018/uploads/visuals/pillar-media.svg"
+                  alt=""
+                />
+                <h4>Media</h4>
+              </div>
+              <div className="partners__gridItem">
+                <img
+                  src="https://s3.amazonaws.com/events.tnw/hardfork-2018/uploads/companies/pillar-events.svg"
+                  alt=""
+                />
+                <h4>Events</h4>
+              </div>
+              <div className="partners__gridItem">
+                <img
+                  src="https://s3.amazonaws.com/events.tnw/hardfork-2018/uploads/visuals/pillar-spaces.svg"
+                  alt=""
+                />
+                <h4>Spaces</h4>
+              </div>
+              <div className="partners__gridItem">
+                <img
+                  src="https://s3.amazonaws.com/events.tnw/hardfork-2018/uploads/companies/pillar-programs.svg"
+                  alt=""
+                />
+                <h4>Programs</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="partners__list">
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/google.svg"
+                alt="Google"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/etoro.svg"
+                alt="Etoro"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/ibm.svg"
+                alt="IBM"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/microsoft.svg"
+                alt="Microsoft"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/paypal.svg"
+                alt="PayPal"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/vodafone.svg"
+                alt="Vodafone"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/accenture.svg"
+                alt="Accenture"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://next.tnwcdn.com/assets/next/img/partners/mckinsey-company.svg"
+                alt="McKinsey & Company"
+              />
+            </a>
+          </div>
+        </section>
       </main>
     </div>
   );
